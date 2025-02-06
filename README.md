f4cd41b8e9e14d2b80e692bd659f0b3795c35f04
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2025-02-06 21:09:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Hospital Management System
+
+The goal of the project is to facilitate health facilities in the midst of pandemic to be
+digital so that they can work more effectively. This app will mainly targets low-end
+health facilities in second-third tier cities.
+## Features
+### Admin
+- Booking / Outpatient (Queue & Status)
+- Add Patient
+- Add Doctor
+- Add Nurse
+- Management Patient
+- Management Doctor
+- Management Nurse
+- Management Schedule
+- Management Session Schedule
+
+### Doctor
+- Add Prescription
+- View Patient Data
+- View Prescription
+- View Booking / Outpatient
+
+### Nurse
+- View Patient Data
+- View Booking / Outpatient
+- View Prescription
+
+## Tech Stack
+- ReactJS
+- Material UI
+- Tailwindcss
+- Redux Persist
+- Axios
+- React Router V6
+
+## Documentation
+- [Figma](https://www.figma.com/file/BW55i7s9OkaxDUxSotQsnY/Hospital-Management-System?node-id=23%3A10866)
+
---
+++ src/hooks/GetDataFacilities.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+export default function GetDataFacilities(refresh) {
+  const API_URL = process.env.REACT_APP_API_URL;
+  const bearerToken = useSelector((state) => state.login.token);
+
+  const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      "Access-Control-Allow-Headers":
+        "append,delete,entries,foreach,get,has,keys,set,values,Authorization",
+      Authorization: `Bearer ${bearerToken}`,
+    },
+  });
+
+  const [dataFacilities, setDataFacilities] = useState({});
+  const [properties, setProperties] = useState({
+    loading: true,
+    error: false,
+  });
+
+  const getDataFacilities = () => {
+    axios.get(`${API_URL}/api/salles`).then((res) => {
+      setDataFacilities(res.data);
+      setProperties({
+        loading: false,
+        error: false,
+      });
+    });
+  };
+
+  useEffect(() => getDataFacilities(), [refresh]);
+
+  return { dataFacilities, getDataFacilities, properties };
+}
